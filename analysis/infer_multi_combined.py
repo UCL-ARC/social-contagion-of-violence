CIN-20 fix average_events and repeat for multiple parameters

analysis/infer_multi_combined.py
@@ -1,116 +1,105 @@
 import numpy as np
 from sklearn.linear_model import LogisticRegression
+from dataclasses import asdict
 import mlflow
-import time
 import os
 
 os.environ['DISPLAY'] = '0'  # Workaround to ensure tick does not overwrite the matplotlib back_end
 from tick.hawkes import SimuHawkesExpKernels
 import src as src
+from input.parameters import params
 
 #######################################################################################################################
 # INITIALISE
 
 setup = dict(
-    start_time=time.time(),
     output_dir=src.set_directory('results/infer_multi_combined/'),
     show_fig=True,
     save_results=False,
     verbose=False,
     percentages=np.array([0.5, 1, 2, 5, 10, 20, 40, 60, 80, 100]),
+    n_realizations=1,
+    n_shuffles=1000,
+    experiment_name='29SEP',
 )
 
-params = dict(
-    training_time=150,  # training time, which could be in hours, days or weeks depending on scenario
-    prediction_time=50,  # testing time
-    average_events=0.2,  # expected number of random events averaged over training time and all nodes
-    contagion=0.1,  # expected number of "direct" triggered events
-    lifetime=5,  # average time between initial and triggered event
-    n_nodes=2000,  # number of nodes
-    network_type='barabasi_albert',  # type of graph, either barabasi_albert or newman_watts_strogatz
-    seed=0,
-    feature_proportions = [0.5,0.5,0.5,0.5,0.5],
-    homophilic=False,
-    feature_variation=2,  # applies a correlation between node features and node risk
-    row=0,  # 0.5,
-    omega=1,  # 0.05,
-    phi=0,  # np.pi,
-)
+for seed in range(setup['n_realizations']):
+    for param_name, param in params.items():
+        #######################################################################################################################
+        # SIMULATE
 
-#######################################################################################################################
-# SIMULATE
+        # Create network and baseline intensity
+        bs = src.SimuBaseline(n_nodes=param.n_nodes, network_type=param.network_type, seed=seed)
+        bs.set_features(average_events=param.average_events, proportions=param.feature_proportions,
+                        variation=param.feature_variation, homophilic=param.homophilic)
+        bs.set_sinusoidal_time(param.run_time, param.row, param.omega, param.phi)
+        bs.plot_node_average(show=setup['show_fig'], filename='baseline_variation.png', directory=setup['output_dir'])
+        bs.plot_network(feature=0, show=setup['show_fig'], filename='network.png', directory=setup['output_dir'])
 
-# Create network and baseline intensity
-bs = src.SimuBaseline(n_nodes=params['n_nodes'], network_type=params['network_type'], seed=params['seed'])
-bs.set_features(average_events=params['average_events'], proportions=params['feature_proportions'],
-                variation=params['feature_variation'], homophilic=params['homophilic'])
-bs.set_sinusoidal_time(params['training_time'] + params['prediction_time'],
-                       params['row'], params['omega'], params['phi'])
-bs.plot_node_average(show=setup['show_fig'], filename='baseline_variation.png', directory=setup['output_dir'])
-bs.plot_network(feature=0, show=setup['show_fig'], filename='network.png', directory=setup['output_dir'])
+        # Generate timestamps
+        simu = SimuHawkesExpKernels(adjacency=param.contagion * bs.adjacency,
+                                    decays=1 / param.lifetime, baseline=bs.time_functions,
+                                    end_time=param.run_time, seed=seed,
+                                    verbose=setup['verbose'], force_simulation=True)
+        simu.track_intensity(intensity_track_step=param.run_time)
+        simu.simulate()
+        t = simu.timestamps
+        src.plot_timestamps(t, show=setup['show_fig'], filename='timestamps.png', directory=setup['output_dir'])
 
-# Generate timestamps
-simu = SimuHawkesExpKernels(adjacency=params['contagion'] * bs.adjacency,
-                            decays=1 / params['lifetime'],
-                            baseline=bs.time_functions,
-                            seed=params['seed'],
-                            end_time=params['training_time'] + params['prediction_time'],
-                            verbose=setup['verbose'],
-                            force_simulation=True)
-simu.simulate()
-t = simu.timestamps
-src.plot_timestamps(t, show=setup['show_fig'], filename='timestamps.png', directory=setup['output_dir'])
+        #######################################################################################################################
+        # INFER AND PREDICT
 
-#######################################################################################################################
-# INFER AND PREDICT
+        infected_nodes = src.get_infected_nodes(t, range(param.training_time, param.run_time))
 
-infected_nodes = src.get_infected_nodes(t, range(params['training_time'],
-                                                 params['training_time'] + params['prediction_time']))
+        # Contagion Model
+        model_contagion = src.HawkesExpKernelIdentical(bs.network, verbose=setup['verbose'])
+        model_contagion.fit(t, training_time=param.training_time, row=param.row, omega=param.omega, phi=param.phi)
+        risk_contagion = model_contagion.predict_proba(range(param.training_time, param.run_time - 1))
+        cm_contagion = src.confusion_matrix(infected_nodes, risk_contagion, setup['percentages'])
 
-# Contagion Model
-model_contagion = src.HawkesExpKernelIdentical(bs.network, verbose=setup['verbose'])
-model_contagion.fit(t, training_time=params['training_time'],
-                    row=params['row'], omega=params['omega'], phi=params['phi'])
-risk_contagion = model_contagion.predict_proba(range(params['training_time'],
-                                                     params['training_time'] + params['prediction_time'] - 1))
-cm_contagion = src.confusion_matrix(infected_nodes, risk_contagion, setup['percentages'])
+        # Demographic Model
+        y_demographic = src.get_infected_nodes(t, (0, param.training_time))[0]
+        model_demographic = LogisticRegression(random_state=seed)
+        model_demographic.fit(bs.features, y_demographic)
+        risk_demographic = np.array([model_demographic.predict_proba(bs.features)[:, 1]
+                                     for _ in range(param.prediction_time - 1)])
+        cm_demographic = src.confusion_matrix(infected_nodes, risk_demographic, setup['percentages'])
 
-# Demographic Model
-y_demographic = src.get_infected_nodes(t, (0, params['training_time']))[0]
-model_demographic = LogisticRegression(random_state=params['seed'])
-model_demographic.fit(bs.features, y_demographic)
-risk_demographic = np.array([model_demographic.predict_proba(bs.features)[:, 1]
-                             for _ in range(params['prediction_time'] - 1)])
-cm_demographic = src.confusion_matrix(infected_nodes, risk_demographic, setup['percentages'])
+        # Combined Model
+        risk_combined = src.norm(risk_contagion) + src.norm(risk_demographic)
+        cm_combined = src.confusion_matrix(infected_nodes, risk_combined, setup['percentages'])
 
-# Combined Model
-risk_combined = risk_contagion[:] / np.sum(risk_contagion, 1)[:, np.newaxis] + \
-                risk_demographic[:] / np.sum(risk_demographic, 1)[:, np.newaxis]
-cm_combined = src.confusion_matrix(infected_nodes, risk_combined, setup['percentages'])
+        # Random Model
+        rng = np.random.default_rng(seed)
+        risk_random = rng.uniform(0, 1, size=[param.prediction_time - 1, param.n_nodes])
+        cm_random = src.confusion_matrix(infected_nodes, risk_random, setup['percentages'])
 
-# Random Model
-rng = np.random.default_rng(params['seed'])
-risk_random = rng.uniform(0, 1, size=[params['prediction_time'] - 1, params['n_nodes']])
-cm_random = src.confusion_matrix(infected_nodes, risk_random, setup['percentages'])
+        src.plot_cdf(
+            {'contagion': cm_contagion, 'demographic': cm_demographic, 'combined': cm_combined, 'random': cm_random},
+            setup['percentages'], time=param.prediction_time - 1,
+            show=setup['show_fig'], filename='cdf.png', directory=setup['output_dir'], )
 
-src.plot_cdf({'contagion': cm_contagion, 'demographic': cm_demographic, 'combined': cm_combined, 'random': cm_random},
-             setup['percentages'], time=params['prediction_time'] - 1,
-             show=setup['show_fig'], filename='cdf.png', directory=setup['output_dir'], )
-setup['end_time'] = time.time()
+        ########################################################################################################################
+        ## SAVE
 
-########################################################################################################################
-## SAVE
+        if setup['save_results']:
+            # Remote
+            # mlflow.set_tracking_uri('databricks')
+            # mlflow.set_experiment('/Users/soumaya.mauthoor.17@ucl.ac.uk/contagion/'+ param.experiment_name'])
 
-if setup['save_results']:
-    # Remote
-    # mlflow.set_tracking_uri('databricks')
-    # mlflow.set_experiment('/Users/soumaya.mauthoor.17@ucl.ac.uk/contagion_simulation')
+            # Locally
+            mlflow.set_tracking_uri('./mlruns')
+            mlflow.set_experiment(setup['experiment_name'])
 
-    # Locally
-    mlflow.set_tracking_uri('./mlruns')
-    mlflow.set_experiment('/infer_multi_combined')
+            with mlflow.start_run(run_name=param_name):
+                mlflow.set_tag('seed', seed)
+                mlflow.log_params(asdict(param))
+                mlflow.log_metric('n_training_jumps', model_contagion.n_training_jumps)
+                mlflow.log_metric('n_testing_jumps', simu.n_total_jumps - model_contagion.n_training_jumps)
+                mlflow.log_metric('contagion_mu', model_contagion.mu)
+                mlflow.log_metric('contagion_alpha', model_contagion.alpha)
+                mlflow.log_metric('contagion_beta', model_contagion.beta)
+                mlflow.log_metrics({f'demographic_{i}': v for i, v in enumerate(model_demographic.coef_[0])})
+                mlflow.log_artifacts(setup['output_dir'])
 
-    with mlflow.start_run():
-        mlflow.log_params(params)
-        mlflow.log_artifacts(setup['output_dir'])
-        mlflow.log_metric('time', setup['end_time'] - setup['start_time'])
+        print(f'{seed}: {param_name}')

analysis/logistic_regression.py
@@ -2,32 +2,44 @@ import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, confusion_matrix
 import matplotlib.pyplot as plt
+import os
+
+os.environ['DISPLAY'] = '0'
+from src import SimuBaseline
+
+# TODO Turn this into an integration test
 
 n = 1000
+features = 5
+p = 0.2
+variation = 1
+average_events = 0.2
 r = 10
+avg_risk = []
 coeffs = []
 scores = []
-scaling = 1
 
 for seed in range(r):
     rng = np.random.default_rng(seed)
-    gender = rng.choice((-1, 1), size=n, p=(0.9,0.1))
-    age = rng.choice((-1, 1), size=n, p=(0.9,0.1))
-    arrests = rng.choice((-1, 1), size=n, p=(0.9,0.1))
-    xb = gender + age + arrests
-    risk = 1 / (1 + np.exp(-xb*scaling))
-    plt.hist(risk); plt.show()
-    y = rng.binomial(n=1, p=risk)
-    x = np.array([gender, age, arrests]).T
-
     model = LogisticRegression(random_state=seed)
-    model.fit(x, y)
-    coeffs.append(np.concatenate([model.coef_[0], model.intercept_]))
-    y_pred = model.predict(x)
+
+    bs = SimuBaseline(n_nodes=n, network_type='path', seed=seed)
+    bs.set_features(proportions=np.ones(features) * p, variation=variation, average_events=average_events)
+    avg_risk.append(np.average(bs.node_average))
+    y = rng.binomial(n=1, p=bs.node_average)
+    model.fit(bs.features, y)
+    y_pred = model.predict(bs.features)
+
+    coeffs.append(model.coef_[0])
     scores.append(accuracy_score(y, y_pred))
 
+plt.hist(bs.sum_features);
+plt.show()
+plt.hist(bs.node_average);
+plt.show()
+plt.scatter(bs.sum_features, bs.node_average);
+plt.show()
+print(np.average(avg_risk))
+# Should be close to average_events
 print(np.average(coeffs, 0), np.std(coeffs, 0))
-# [0.93457335 1.01115506 1.01225805 0.01431867] [0.11067048 0.10161891 0.10335749 0.10000723]
-print(np.average(scores, 0), np.std(scores, 0))
-# 0.8924 0.008475848040166843
-
+# Should be close to variation

src/hawkesexpkernelidentical.py
@@ -15,7 +15,7 @@ class HawkesExpKernelIdentical:
         self.alpha_range = None
         self.mu_range = None
         self.beta_range = None
-        self.n_total_jumps = None
+        self.n_training_jumps = None
         self.mu_average = None
         self.coef_ = None
         self.mu = None
@@ -29,8 +29,8 @@ class HawkesExpKernelIdentical:
             raise ValueError('length of timestamps should be the same as number of nodes')
         self.timestamps = timestamps
         self.training_time = training_time
-        self.n_total_jumps = np.sum(np.concatenate(timestamps)<self.training_time)
-        self.mu_average = self.n_total_jumps / (self.n_nodes * self.training_time)
+        self.n_training_jumps = np.sum(np.concatenate(timestamps) < self.training_time)
+        self.mu_average = self.n_training_jumps / (self.n_nodes * self.training_time)
         self._set_ranges()
         brute_res = brute(self._ll_multi, full_output=True, finish=0, Ns=5,
                           ranges=(self.mu_range, self.alpha_range, self.beta_range),
@@ -38,16 +38,21 @@ class HawkesExpKernelIdentical:
         ggd_res = minimize(self._ll_multi, x0=brute_res[0], method='L-BFGS-B',
                            bounds=(self.mu_range, self.alpha_range, self.beta_range),
                            args=(self.network, timestamps, self.training_time, row, omega, phi, self.verbose))
+        self.brute_result = brute_res
         self.optimise_result = ggd_res
         self.coef_ = ggd_res.x
         self.mu = ggd_res.x[0]
-        self.alpha = ggd_res.x[1]
-        self.beta = ggd_res.x[2]
+        if ggd_res.x[1] < self.alpha_range[1] and ggd_res.x[2] > self.beta_range[0]:
+            self.alpha = ggd_res.x[1]
+            self.beta = ggd_res.x[2]
+        else:
+            self.alpha = 1e-10
+            self.beta = 0
 
     def _set_ranges(self):
         self.mu_range = (1e-10, 2 * self.mu_average)
-        self.alpha_range = (1e-10, 0.5)
-        self.beta_range = (1e-10, 1)
+        self.alpha_range = (1e-10, 1)
+        self.beta_range = (1 / (self.training_time * 5), 1)
 
     def _recursive(self, timestamps, beta, ):
         r_array = np.zeros(len(timestamps))

src/simubaseline.py
@@ -17,6 +17,7 @@ class SimuBaseline:
         self.assortativity = None
         self.features = None
         self.node_average = None
+        self.sum_features = None
         self.time_functions = None
         self._set_network()
         self._set_adjacency()
@@ -39,7 +40,7 @@ class SimuBaseline:
     def _set_adjacency(self):
         self.adjacency = nx.to_numpy_array(self.network)
 
-    def _homophilic_feature(self, values=None, node_centers=None, max_nodes=None, base=-1, ):
+    def _homophilic_feature(self, values=None, node_centers=None, max_nodes=None, base=0, ):
         node_list = []
         node_attribute = {i: base for i in self.network.nodes}
         if max_nodes is None:
@@ -100,19 +101,27 @@ class SimuBaseline:
         ut.enhance_plot(fig, show, filename, params_dict, directory)
 
     def set_features(self, proportions, variation, average_events, homophilic=False):
-        arr = np.zeros([len(proportions), self.n_nodes])
+
+        if homophilic:
+            self.features = np.zeros(([self.n_nodes, len(proportions)]))
+            for i, p in enumerate(proportions):
+                self.features[:, i] = self._homophilic_feature(values=[1], max_nodes=int(self.n_nodes * p))
+        else:
+            self.features = self.rng.binomial(n=1, p=proportions, size=(self.n_nodes, len(proportions)))
+
+        self.sum_features = np.sum(self.features - np.average(self.features), 1)
+        log_shift = -np.log(1 / average_events - 1)
+        # The error is a work-around to ensure that the average of the node_average is equal to the average_events
+        # This is probably caused by floating-point errors
+        # Note that this fix is not enough for large variations (>1.5)
+        error = np.average(1 / (1 + np.exp(-(self.sum_features * variation + log_shift)))) - average_events
+        log_shift_error = -np.log(1 / (average_events-error) - 1)
+        self.node_average = 1 / (1 + np.exp(-(self.sum_features * variation + log_shift_error)))
+
         self.assortativity = np.zeros(len(proportions))
-        for i, feature in enumerate(proportions):
-            if homophilic:
-                values = self._homophilic_feature(values=[1], max_nodes=int(self.n_nodes * feature))
-            else:
-                values = self.rng.choice(a=(-1, 1), size=self.n_nodes, p=(1 - feature, feature))
-            nx.set_node_attributes(self.network, {i: k for i, k in enumerate(values)}, i)
+        for i in range(len(proportions)):
+            nx.set_node_attributes(self.network, dict(enumerate(self.features[:, i])), i)
             self.assortativity[i] = nx.attribute_assortativity_coefficient(self.network, i)
-            arr[i] = values
-        self.features = arr.T
-        logit = 1 / (1 + np.exp(-np.sum(self.features, 1) * variation))
-        self.node_average = logit * self.n_nodes * average_events / sum(logit)
 
     def set_sinusoidal_time(self, end_time, row, omega, phi, num=100):
         t_values = np.linspace(0, end_time, num=num)

tests/test_simubaseline.py
@@ -11,37 +11,37 @@ def test_SimuBaseline():
 def test_homophily():
     bs = SimuBaseline(n_nodes=4, network_type='path', )
     feature = bs._homophilic_feature(values=[1], node_centers=[0], )
-    np.testing.assert_array_equal(feature, np.array([1, 1, -1, -1]))
+    np.testing.assert_array_equal(feature, np.array([1, 1, 0, 0]))
 
 
 def test_homophily_max():
     bs = SimuBaseline(n_nodes=4, network_type='star', )
     feature = bs._homophilic_feature(values=[1], node_centers=[0], max_nodes=2)
-    np.testing.assert_array_equal(feature, np.array([1, 1, -1, -1, -1]))
+    np.testing.assert_array_equal(feature, np.array([1, 1, 0, 0, 0]))
 
     bs = SimuBaseline(n_nodes=6, network_type='path', seed=0)
     feature = bs._homophilic_feature(values=[1], node_centers=[1, 4], max_nodes=5)
-    np.testing.assert_array_equal(feature, np.array([1, 1, 1, 1, 1, -1]))
+    np.testing.assert_array_equal(feature, np.array([1, 1, 1, 1, 1, 0]))
 
 
 def test_features():
-    bs = SimuBaseline(n_nodes=4, network_type='path', seed=0)
-    bs.set_features(proportions=[0.5], average_events=2, variation=1)
-    np.testing.assert_array_equal(bs.features.T, [[1., -1., -1., -1.]])
-    assert 2 == pytest.approx(np.sum(bs.node_average) / bs.n_nodes)
+    bs = SimuBaseline(n_nodes=5, network_type='path', seed=0)
+    bs.set_features(proportions=[0.5], average_events=0.5, variation=1)
+    np.testing.assert_array_equal(bs.features.T, [[1., 0., 0., 0.,1]])
+    assert 0.5 == pytest.approx(np.sum(bs.node_average) / bs.n_nodes, rel=1e-2)
 
 
 def test_features_multi():
     bs = SimuBaseline(n_nodes=3, network_type='path', seed=0)
-    bs.set_features(proportions=[0.5, 0.5], average_events=2, variation=1)
-    np.testing.assert_array_equal(bs.features.T, [[1., -1., -1.], [-1., 1., 1.]])
-    assert 2 == pytest.approx(np.sum(bs.node_average) / bs.n_nodes)
-    np.testing.assert_array_almost_equal(bs.assortativity * 3, [-1, -1])
+    bs.set_features(proportions=[0.5, 0.5], average_events=0.5, variation=1)
+    np.testing.assert_array_equal(bs.features.T, [[1., 0, 1.], [0., 0., 1.]])
+    assert 0.5 == pytest.approx(np.sum(bs.node_average) / bs.n_nodes, rel=1e-2)
+    np.testing.assert_array_almost_equal(bs.assortativity * 3, [-3, -1])
 
 
 def test_features_homophilic():
     bs = SimuBaseline(n_nodes=3, network_type='path', seed=0)
-    bs.set_features(proportions=[0.5, 0.5], average_events=2, variation=1, homophilic=True)
-    np.testing.assert_array_equal(bs.features.T, [[-1., -1., 1.], [-1., 1., -1.]])
-    assert 2 == pytest.approx(np.sum(bs.node_average) / bs.n_nodes)
+    bs.set_features(proportions=[0.5, 0.5], average_events=0.5, variation=1, homophilic=True)
+    np.testing.assert_array_equal(bs.features.T, [[0., 0., 1.], [0., 1., 0.]])
+    assert 0.5 == pytest.approx(np.sum(bs.node_average) / bs.n_nodes,rel=1e-2)
     np.testing.assert_array_almost_equal(bs.assortativity * 3, [-1, -3])