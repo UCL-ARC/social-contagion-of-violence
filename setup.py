a34d0929cd1e7f84564f06045f802160fae00e46
Ed Lowther <edlowther@users.noreply.github.com>
2022-02-09 15:14:53 +0000

    Updates version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='social-contagion-of-violence',
-   version='1.0.1',
+   version='1.0',
    description='A Python-based framework to simulate, analyse and infer infections in a network assuming a Hawkes contagion process and accounting for various confounding effects.',
    author='Soumaya Mauthoor',
    author_email='e.lowther@ucl.ac.uk',
