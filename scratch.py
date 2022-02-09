cb7d0a63f5b01f48b5c526744cc10742c2c2d898
Ed Lowther <edlowther@users.noreply.github.com>
2022-02-09 15:04:16 +0000

    Adds version number

---
+++ scratch.py
@@ -0,0 +1,4 @@
+import src
+from src.input.simulationparams import simu_params
+
+print(simu_params)
--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup
+from src.version import __version__
 
 setup(
    name='social-contagion-of-violence',
-   version='1.0',
+   version=__version__,
    description='A Python-based framework to simulate, analyse and infer infections in a network assuming a Hawkes contagion process and accounting for various confounding effects.',
    author='Soumaya Mauthoor',
    author_email='e.lowther@ucl.ac.uk',
