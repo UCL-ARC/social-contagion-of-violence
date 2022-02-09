fe55f968a89e1b47804fb08fe2bc701ed7d41030
Ed Lowther <edlowther@users.noreply.github.com>
2022-02-09 11:01:46 +0000

    Goes back to original package name

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
    description='A Python-based framework to simulate, analyse and infer infections in a network assuming a Hawkes contagion process and accounting for various confounding effects.',
    author='Soumaya Mauthoor',
    author_email='e.lowther@ucl.ac.uk',
-   packages=['contagion'],
+   packages=['src'],
    install_requires=['tick', 'networkx'],
 )
