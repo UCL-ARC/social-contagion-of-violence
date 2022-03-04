8deb0a3532c5471583e4c0e5fe40ad9f2d25931d
Ed Lowther <edlowther@users.noreply.github.com>
2022-03-04 17:17:48 +0000

    Adds draft method of plugging dummy data into Hawkes model

--- dummy-data.py
+++ dummy-data.py
@@ -2,7 +2,10 @@ from datetime import datetime, timedelta
 from random import choice, choices, randrange
 
 import pandas as pd
+import numpy as np
 import networkx as nx
+
+import src
 
 all_person_ids = [f'OCC_PERSON_OVERALL_ID_{x:03}' for x in range(50)]
 all_occurrence_ids = [f'OCCURRENCE_ID_{x:03}' for x in range(25)]
@@ -15,7 +18,7 @@ def random_date():
     random_second = randrange(int_delta)
     return start_date + timedelta(seconds=random_second)
 
-n = 200
+n = 100
 
 person_ids       = [choice(all_person_ids) for _ in range(n)]
 occurrence_ids   = [choice(all_occurrence_ids) for _ in range(n)]
@@ -32,3 +35,18 @@ G = nx.bipartite.projected_graph(B, dummy_df['ID_LINKED_ON_NICHE_PNC_NAME_DOB_PO
 
 print(G.order())
 print(len(dummy_df['ID_LINKED_ON_NICHE_PNC_NAME_DOB_POSTCODE'].unique()))
+
+dummy_df['DAY_INT'] = (dummy_df['REPORTED_DATE'].max() - dummy_df['REPORTED_DATE']).dt.days
+
+train_test_threshold = int(np.quantile(dummy_df['DAY_INT'], 0.7))
+last_day_in_data = dummy_df['DAY_INT'].max()
+
+t = []
+for n in G: 
+    mask = (dummy_df['ID_LINKED_ON_NICHE_PNC_NAME_DOB_POSTCODE']  == n) & (dummy_df['VIOLENT_CRIME_FLAG'])
+    t.append(np.array(dummy_df.loc[mask, 'DAY_INT']))
+
+model_contagion = src.HawkesExpKernelIdentical(G, verbose=True)
+model_contagion.fit(t, training_time=train_test_threshold, row=0, omega=1, phi=0)
+preds = model_contagion.predict_proba(range(train_test_threshold, last_day_in_data))
+print(preds)
